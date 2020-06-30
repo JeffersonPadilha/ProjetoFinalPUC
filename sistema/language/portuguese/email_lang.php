8e45e0aa578b5aea33cdff70751a67f22a4ed985
JeffersonPadilha <jeffersonpadilhadossantos@gmail.com>
2020-06-30 14:53:42 -0300

    Create email_lang.php

---
+++ sistema/language/portuguese/email_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang ['email_must_be_array'] = "O método de validação de email deve receber uma matriz.";
+$lang ['email_invalid_address'] = "Endereço de email inválido: %s";
+$lang ['email_attachment_missing'] = "Não foi possível localizar o seguinte anexo de email: %s";
+$lang ['email_attachment_unreadable'] = "Não foi possível abrir este anexo: %s";
+$lang ['email_no_recipients'] = "Você deve incluir os destinatários: Para, Cc ou Cco";
+$lang ['email_send_failure_phpmail'] = "Não foi possível enviar o email usando PHP mail (). Seu servidor pode não estar configurado para enviar email usando este método.";
+$lang ['email_send_failure_sendmail'] = "Não foi possível enviar email usando o PHP Sendmail. Seu servidor pode não estar configurado para enviar email usando este método.";
+$lang ['email_send_failure_smtp'] = "Não foi possível enviar o email usando PHP SMTP. Seu servidor pode não estar configurado para enviar email usando este método.";
+$lang ['email_sent'] = "Sua mensagem foi enviada com sucesso usando o seguinte protocolo: %s";
+$lang ['email_no_socket'] = "Não foi possível abrir um soquete para o Sendmail. Verifique as configurações.";
+$lang ['email_no_hostname'] = "Você não especificou um nome de host SMTP.";
+$lang ['email_smtp_error'] = "O seguinte erro SMTP foi encontrado: %s";
+$lang ['email_no_smtp_unpw'] = "Erro: você deve atribuir um nome de usuário e senha SMTP.";
+$lang ['email_failed_smtp_login'] = "Falha ao enviar o comando AUTH LOGIN. Erro: %s";
+$lang ['email_smtp_auth_un'] = "Falha ao autenticar o nome de usuário. Erro: %s";
+$lang ['email_smtp_auth_pw'] = "Falha ao autenticar a senha. Erro: %s";
+$lang ['email_smtp_data_failure'] = "Não foi possível enviar os dados: %s";
+$lang ['email_exit_status'] = "Código de status da saída: %s";
