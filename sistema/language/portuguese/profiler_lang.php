73309e2292985b7b34c90522d73406c881a892ac
JeffersonPadilha <jeffersonpadilhadossantos@gmail.com>
2020-06-30 14:53:26 -0300

    Create profiler_lang.php

---
+++ sistema/language/portuguese/profiler_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['profiler_database']		= 'DATABASE';
+$lang['profiler_controller_info'] = 'CLASS/METHOD';
+$lang['profiler_benchmarks']	= 'BENCHMARKS';
+$lang['profiler_queries']		= 'QUERIES';
+$lang['profiler_get_data']		= 'GET DATA';
+$lang['profiler_post_data']		= 'POST DATA';
+$lang['profiler_uri_string']	= 'URI STRING';
+$lang['profiler_memory_usage']	= 'MEMORY USAGE';
+$lang['profiler_config']		= 'CONFIG VARIABLES';
+$lang['profiler_session_data']	= 'SESSION DATA';
+$lang['profiler_headers']		= 'HTTP HEADERS';
+$lang ['profiler_no_db'] 		= 'O driver do banco de dados não está carregado no momento';
+$lang ['profiler_no_queries'] 	= 'Nenhuma consulta foi executada';
+$lang ['profiler_no_post'] 		= 'Não existem dados do POST';
+$lang ['profiler_no_get']		= 'Não existem dados GET';
+$lang ['profiler_no_uri'] 		= 'Não existem dados URI';
+$lang ['profiler_no_memory'] 	= 'Uso da memória indisponível';
+$lang ['profiler_no_profiles'] 	= 'Sem dados do perfil - todas as seções do Profiler foram desativadas.';
+$lang['profiler_section_hide']	= 'Hide';
+$lang['profiler_section_show']	= 'Show';
