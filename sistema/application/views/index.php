a56efdce42f0482461cd5b0d6d4d2a64bf4baad5
JeffersonPadilha <jeffersonpadilhadossantos@gmail.com>
2020-06-30 14:54:43 -0300

    Create index.php

---
+++ sistema/application/views/index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE>
+<html lang="pt-br">
+    <head>
+        <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1, user-scalable=0">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+		<?php include 'includes.php';?>
+        <title>COD System</title>
+    </head>
+    
+    
+<body>
+    <?php include 'header.php';?>
+    <?php include $this->session->userdata('login_type').'/navigation.php';?>
+    <div class="main-content">
+		<?php include 'page_info.php';?>
+        <div class="container-fluid padded">
+            <?php include $this->session->userdata('login_type').'/'.$page_name.'.php';?>
+        </div>        
+	<?php include 'footer.php';?>
+    </div>
+    
+</body>
+</html>
