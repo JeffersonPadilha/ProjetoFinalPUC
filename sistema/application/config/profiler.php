193d0241c9f64e56dc4b99c753724fbe96be1dc5
JeffersonPadilha <jeffersonpadilhadossantos@gmail.com>
2020-06-30 14:57:15 -0300

    Create profiler.php

---
+++ sistema/application/config/profiler.php
@@ -0,0 +1,17 @@
+<?php  if ( ! defined('BASEPATH')) exit('Acesso nao permitido!');
+/*
+| -------------------------------------------------- -----------------------
+| Seções do Profiler
+| -------------------------------------------------- -----------------------
+| Esse arquivo permite determinar se várias seções do Profiler são ou não
+| os dados são exibidos quando o Profiler está ativado.
+| Consulte o guia do usuário para obter informações:
+|
+| http://codeigniter.com/user_guide/general/profiling.html
+|
+*/
+
+
+
+/* Fim do arquivo profiler.php */
+/* Local: ./application/config/profiler.php */
