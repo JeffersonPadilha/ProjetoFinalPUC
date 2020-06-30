8a0f6f5a6628bd1676e635ba1024d5609db6e690
JeffersonPadilha <jeffersonpadilhadossantos@gmail.com>
2020-06-30 14:56:11 -0300

    Create unit_test_lang.php

---
+++ sistema/application/language/portuguese/unit_test_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang ['ut_test_name'] = 'Nome do teste';
+$lang ['ut_test_datatype'] = 'Testar tipo de dados';
+$lang ['ut_res_datatype'] = 'Tipo de dados esperado';
+$lang ['ut_result'] = 'Resultado';
+$lang ['ut_undefined'] = 'Nome indefinido do teste';
+$lang ['ut_file'] = 'Nome do arquivo';
+$lang ['ut_line'] = 'Número da linha';
+$lang ['ut_passed'] = 'Aprovado';
+$lang ['ut_failed'] = 'Falha';
+$lang ['ut_boolean'] = 'Booleano';
+$lang ['ut_integer'] = 'Inteiro';
+$lang ['ut_float'] = 'Flutuante';
+$lang ['ut_double'] = 'Flutuante'; // pode ser o mesmo que float
+$lang ['ut_string'] = 'String';
+$lang ['ut_array'] = 'Matriz';
+$lang ['ut_object'] = 'Objeto';
+$lang ['ut_resource'] = 'Recurso';
+$lang ['ut_null'] = 'Nulo';
+$lang ['ut_notes'] = 'Notas';
