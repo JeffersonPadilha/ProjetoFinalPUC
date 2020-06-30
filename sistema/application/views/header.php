6ad0dfa536748af724e311c4876f2d30a012f032
JeffersonPadilha <jeffersonpadilhadossantos@gmail.com>
2020-06-30 14:54:50 -0300

    Create header.php

---
+++ sistema/application/views/header.php
@@ -0,0 +1,40 @@
+<div class="navbar navbar-top navbar-inverse">
+	<div class="navbar-inner">
+		<div class="container-fluid">
+			<a class="brand" href="<?php echo base_url();?>"><?php echo $system_name;?>
+			</a>
+			<!-- novos botões de alternância -->
+			<ul class="nav pull-right">
+				<li class="toggle-primary-sidebar hidden-desktop" data-toggle="collapse" data-target=".nav-collapse-primary"><button type="button" class="btn btn-navbar"><i class="icon-th-list"></i></button></li>
+				<li class="hidden-desktop" data-toggle="collapse" data-target=".nav-collapse-top"><button type="button" class="btn btn-navbar"><i class="icon-align-justify"></i></button></li>
+			</ul>
+			<div class="nav-collapse nav-collapse-top collapse">
+				<ul class="nav pull-right">
+					<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Conta <b class="caret"></b></a>
+					<!-- Seletor de conta -->
+                    <ul class="dropdown-menu">
+                    	<li class="with-image">
+                        	<span>
+                            <?php 
+								$account_type	=	$this->session->userdata('login_type');
+								$account_id		=	$account_type.'_id';
+								$name			=	$this->crud_model->get_type_name_by_id($account_type , $this->session->userdata($account_id) , 'nome');
+								echo $name;
+							?>
+                            </span>
+                        </li>
+                    	<li class="divider"></li>
+						<li><a href="<?php echo base_url();?>index.php?<?php echo $this->session->userdata('login_type');?>/gerenciar_perfil">
+                        		<i class="icon-user"></i><span>Perfil</span></a></li>
+						<li><a href="<?php echo base_url();?>index.php?login/logout">
+                        		<i class="icon-off"></i><span>Sair</span></a></li>
+					</ul>
+                	<!-- Seletor de conta -->
+					</li>
+				</ul>
+
+			</div>
+		</div>
+	</div>
+</div>
