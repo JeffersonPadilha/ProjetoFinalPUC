4d0655e0537bb546a8444adf0d762cc2ae2c5a74
JeffersonPadilha <jeffersonpadilhadossantos@gmail.com>
2020-06-30 14:57:27 -0300

    Create hooks.php

---
+++ sistema/application/config/hooks.php
@@ -0,0 +1,16 @@
+<?php  if ( ! defined('BASEPATH')) exit('Acesso nao permitido!');
+/*
+| -------------------------------------------------- -----------------------
+| Hooks
+| -------------------------------------------------- -----------------------
+| Esse arquivo permite definir "path" para estender o IC sem invadir os arquivos core.
+| Consulte o guia do usuário para obter informações:
+|
+| http://codeigniter.com/user_guide/general/hooks.html
+|
+*/
+
+
+
+/* Fim do arquivo hooks.php * /
+/* Local: ./application/config/hooks.php * /
