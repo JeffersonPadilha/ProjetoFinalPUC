78d2d805191900d171b569748e70da1e8999b019
JeffersonPadilha <jeffersonpadilhadossantos@gmail.com>
2020-06-30 14:55:53 -0300

    Create language.php

---
+++ sistema/application/models/language.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('Acesso nao permitido!');
+
+class Language extends CI_Model {
+	
+	function __construct()
+    {
+        parent::__construct();
+    }
+	
+	function language($email , $password)
+	{
+
+	}
+	
+	
+}
+
