b4ad12cd562f3c4ffa25ac006f23f8f293aa5bb7
JeffersonPadilha <jeffersonpadilhadossantos@gmail.com>
2020-06-30 14:53:30 -0300

    Create migration_lang.php

---
+++ sistema/language/portuguese/migration_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang ['migration_none_found'] 			= "Nenhuma migração foi encontrada.";
+$lang ['migration_not_found'] 			= "Esta migração não foi encontrada.";
+$lang ['migration_multiple_version'] 	= "São várias migrações com o mesmo número de versão:%d.";
+$lang['migration_class_doesnt_exist']	= "A classe de migração \"%s\" nao foi encontrada.";
+$lang['migration_missing_up_method']	= "A classe de migração \"%s\" está faltando um método 'up'.";
+$lang['migration_missing_down_method']	= "A classe de migração \"%s\" está faltando um método 'down'.";
+$lang['migration_invalid_filename']		= "Migraçao \"%s\" tem um nome de arquivo inválido.";
+
