209b58b3f4e0cddb7c9f395a2b269af0e6e9b42e
JeffersonPadilha <jeffersonpadilhadossantos@gmail.com>
2020-06-30 14:54:06 -0300

    Create ftp_lang.php

---
+++ sistema/language/english/ftp_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang ['ftp_no_connection'] = "Não foi possível localizar um ID de conexão válido. Verifique se você está conectado antes de executar as rotinas de arquivos.";
+$lang ['ftp_unable_to_connect'] = "Não foi possível conectar ao seu servidor FTP usando o nome do host fornecido.";
+$lang ['ftp_unable_to_login'] = "Não foi possível fazer login no seu servidor FTP. Por favor, verifique seu nome de usuário e senha.";
+$lang ['ftp_unable_to_makdir'] = "Não foi possível criar o diretório que você especificou.";
+$lang ['ftp_unable_to_changedir'] = "Não foi possível alterar os diretórios.";
+$lang ['ftp_unable_to_chmod'] = "Não foi possível definir as permissões do arquivo. Verifique o seu caminho. Nota: Esse recurso está disponível apenas no PHP 5 ou superior.";
+$lang ['ftp_unable_to_upload'] = "Não foi possível carregar o arquivo especificado. Por favor, verifique seu caminho.";
+$lang ['ftp_unable_to_download'] = "Não foi possível baixar o arquivo especificado. Verifique seu caminho.";
+$lang ['ftp_no_source_file'] = "Não foi possível localizar o arquivo de origem. Verifique o seu caminho.";
+$lang ['ftp_unable_to_rename'] = "Não foi possível renomear o arquivo.";
+$lang ['ftp_unable_to_delete'] = "Não foi possível excluir o arquivo.";
+$lang ['ftp_unable_to_move'] = "Não foi possível mover o arquivo. Verifique se o diretório de destino existe.";
